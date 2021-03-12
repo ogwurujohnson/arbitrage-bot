d3e1a0ba8faf37875ae71358f69345e848cc565e
Johnson Ogwuru <ogwurujohnson@gmail.com>
2021-03-12 23:52:48 +0100

    establish connection to chain

---
+++ index.ts
@@ -0,0 +1,21 @@
+import { config } from 'dotenv'
+import { ethers } from 'ethers'
+
+// uni/sushiswap/pancake/bakery ABIs
+import UniswapV2Pair from './abis/IUniswapV2Pair.json'
+import UniswapV2Factory from './abis/IUniswapV2Factory.json'
+
+config()
+
+const { 
+  PRIVATE_KEY: privateKey, 
+  FLASH_LOANER: flashLoanerAddress,
+  RPC_LINK: rpcLink
+} = process.env;
+
+const provider = new ethers.providers.JsonRpcProvider(rpcLink)
+
+const wallet = new ethers.Wallet(privateKey, provider);
+
+const ETH_TRADE = 10;
+const DAI_TRADE = 3500
