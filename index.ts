4b96ea4d4eac0df9f4b3fa9e232f2d066657c64f
Johnson Ogwuru <ogwurujohnson@gmail.com>
2021-03-13 05:10:52 +0100

    calculate gas, and enforce that application terminates when gas price is greater than spread

--- index.ts
+++ index.ts
@@ -60,7 +60,47 @@ const startBot = async () => {
       const reserve0Pancake = Number(ethers.utils.formatUnits(pancakeReserves[0], 18))
       const reserve1Pancake = Number(ethers.utils.formatUnits(pancakeReserves[1], 18))
 
-      
+      const pricePancakeSwap = reserve0Pancake / reserve1Pancake
+      const priceBakerySwap = reserve0Bakery / reserve1Bakery
+
+      const shouldStartEth = pricePancakeSwap < priceBakerySwap
+      const spread = Math.abs((priceBakerySwap / pricePancakeSwap - 1) * 100) - 0.6
+
+      const shouldTrade = spread > (
+        (shouldStartEth ? ETH_TRADE : DAI_TRADE)
+        / Number(
+          ethers.utils.formatEther(pancakeReserves[shouldStartEth ? 1 : 0])
+        )
+      )
+
+      console.log(`PANCAKESWAP PRICE ${pricePancakeSwap}`);
+      console.log(`BAKERYSWAP PRICE ${priceBakerySwap}`);
+      console.log(`PROFITABLE? ${shouldTrade}`);
+      console.log(`CURRENT SPREAD: ${(priceBakerySwap / pricePancakeSwap - 1) * 100}%`);
+      console.log(`ABSOLUTE SPREAD: ${spread}`);
+
+      if (!shouldTrade) return
+
+      // should be pancakeETHDai
+      const gasLimit = await bakeryEthDai.estimateGas.swap(
+        !shouldStartEth ? DAI_TRADE : 0,
+        shouldStartEth ? ETH_TRADE : 0,
+        flashLoanerAddress,
+        ethers.utils.toUtf8Bytes('1'),
+      );
+
+      const gasPrice = await wallet.getGasPrice()
+
+      const gasCost = Number(ethers.utils.formatEther(gasPrice.mul(gasLimit)))
+
+      const shouldSendTx = shouldStartEth
+        ? (gasCost / ETH_TRADE) < spread
+        : (gasCost / (DAI_TRADE / pricePancakeSwap)) < spread
+
+      // don't trade if gasCost is higher than the spread
+      if (!shouldSendTx) return;
+
+
     } catch (err) {
       console.log(err);
     }
